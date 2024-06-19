a4623af132764f41cd1d0d1cfb8bc58e789331c1
Elena Amosova <lesoma@me.com>
2024-06-19 09:12:42 -0400

    Refactor tests to use helper functions from helpers moduleSwitched WebDriver from Edge to Chrome for tests

---
+++ helpers/urls.py
@@ -0,0 +1,7 @@
+# helpers/urls.py
+
+BASE_URL = "https://stellarburgers.nomoreparties.site"
+LOGIN_URL = f"{BASE_URL}/login"
+FORGOT_PASSWORD_URL = f"{BASE_URL}/forgot-password"
+RESET_PASSWORD_URL = f"{BASE_URL}/reset-password"
+HOME_URL = BASE_URL  # Assuming the home URL is the base URL
