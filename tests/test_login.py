24b6465b00d31838acd23559aee51785f8501557
Elena Amosova <lesoma@me.com>
2024-06-19 09:28:37 -0400

    Remove try/except blocks from tests and use assertions

--- tests/test_login.py
+++ tests/test_login.py
@@ -17,75 +17,66 @@ def test_positive_login(browser):
 
     browser.get(LOGIN_URL)  # Использование URL из helpers.urls
 
-    try:
-        # Ввод email
-        email_input = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//label[text()='Email']/following-sibling::input"))
-        )
-        email_input.clear()
-        email_input.send_keys(email)
-        assert email_input.get_attribute("value") == email
+    # Ввод email
+    email_input = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//label[text()='Email']/following-sibling::input"))
+    )
+    email_input.clear()
+    email_input.send_keys(email)
+    assert email_input.get_attribute("value") == email
 
-        # Ввод пароля
-        password_input = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//label[text()='Пароль']/following-sibling::input"))
-        )
-        password_input.clear()
-        password_input.send_keys(password)
-        assert password_input.get_attribute("value") == password
+    # Ввод пароля
+    password_input = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//label[text()='Пароль']/following-sibling::input"))
+    )
+    password_input.clear()
+    password_input.send_keys(password)
+    assert password_input.get_attribute("value") == password
 
-        # Нажать кнопку "Войти"
-        login_button = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[text()='Войти']"))
-        )
-        login_button.click()
+    # Нажать кнопку "Войти"
+    login_button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='Войти']"))
+    )
+    login_button.click()
 
-        # Проверка успешного входа
-        WebDriverWait(browser, 20).until(
-            EC.url_contains(HOME_URL)
-        )
-        assert HOME_URL in browser.current_url
-    except TimeoutException:
-        pytest.fail("Login was expected to succeed, but it did not.")
-
+    # Проверка успешного входа
+    WebDriverWait(browser, 20).until(
+        EC.url_contains(HOME_URL)
+    )
+    assert HOME_URL in browser.current_url
 def test_negative_login(browser):
     email = generate_random_email()
     password = generate_random_password()
 
     browser.get(LOGIN_URL)  # Использование URL из helpers.urls
 
-    try:
-        # Ввод email
-        email_input = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//label[text()='Email']/following-sibling::input"))
-        )
-        email_input.clear()
-        email_input.send_keys(email)
-        assert email_input.get_attribute("value") == email
+    # Ввод email
+    email_input = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//label[text()='Email']/following-sibling::input"))
+    )
+    email_input.clear()
+    email_input.send_keys(email)
+    assert email_input.get_attribute("value") == email
 
-        # Ввод пароля
-        password_input = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//label[text()='Пароль']/following-sibling::input"))
-        )
-        password_input.clear()
-        password_input.send_keys(password)
-        assert password_input.get_attribute("value") == password
+    # Ввод пароля
+    password_input = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//label[text()='Пароль']/following-sibling::input"))
+    )
+    password_input.clear()
+    password_input.send_keys(password)
+    assert password_input.get_attribute("value") == password
 
-        # Нажать кнопку "Войти"
-        login_button = WebDriverWait(browser, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[text()='Войти']"))
-        )
-        login_button.click()
+    # Нажать кнопку "Войти"
+    login_button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='Войти']"))
+    )
+    login_button.click()
 
-        # Проверка неуспешного входа
-        WebDriverWait(browser, 10).until(
-            EC.url_to_be(LOGIN_URL)
-        )
-        assert browser.current_url == LOGIN_URL
-
-    except TimeoutException:
-        pytest.fail("Login was expected to fail, but it did not.")
-
+    # Проверка неуспешного входа
+    WebDriverWait(browser, 10).until(
+        EC.url_to_be(LOGIN_URL)
+    )
+    assert browser.current_url == LOGIN_URL
 
 
 
